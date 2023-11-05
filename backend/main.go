38d622f386c32acbd7db54d62a95f8be64eda7e9
Max Gobeil <gobmaxime@gmail.com>
2023-11-04 20:40:01 -0400

    database connection

---
+++ backend/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"example/backend/initializers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDB()
+}
+
+func main(){
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context){
+		c.JSON(200, gin.H{
+			"message": "Up and Running",
+		})
+	})
+	r.Run()
+}
