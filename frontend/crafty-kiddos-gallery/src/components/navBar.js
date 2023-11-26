01a5bbfd2c429fe7b00086ce51c4854a73bd0b3f
Max Gobeil <gobmaxime@gmail.com>
2023-11-26 15:24:21 -0500

    logout cookie again

--- frontend/crafty-kiddos-gallery/src/components/navBar.js
+++ frontend/crafty-kiddos-gallery/src/components/navBar.js
@@ -33,7 +33,7 @@ export function NavBar() {
     setLoginModalOpen(false);
   };
   const handleLogout = () => {
-    document.cookie = `Authorization=; Max-Age=-1; path=/; domain:https://crafty-kiddos-gallery.vercel.app`;
+    document.cookie = `Authorization=; Max-Age=-1; path=/; domain:https://localhost:3000; SameSite=None; Secure`;
     window.location.reload();
   };
 
