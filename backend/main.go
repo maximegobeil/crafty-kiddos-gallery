93ffe0eb223131a8a2a413ee335d5ddeb5f46cf6
Max Gobeil <gobmaxime@gmail.com>
2023-11-30 21:28:49 -0500

    cors

--- backend/main.go
+++ backend/main.go
@@ -17,7 +17,7 @@ func init() {
 func main(){
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
+		AllowOrigins: []string{"https://crafty-kiddos-gallery.vercel.app/"},
 		AllowMethods: []string{"PUT", "PATCH", "POST", "DELETE", "GET"},
 		AllowHeaders: []string{"Content-Type, Authorization"},
 		AllowCredentials: true,
